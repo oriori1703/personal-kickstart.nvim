29d20db250886ab4eefca8d39fbe27fdd9faf803
orip <oriori1703@gmail.com>
2024-10-26 11:51:39 +0300

    Switch from tokyonight to catppuccin

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'catppuccin-mocha', 'catppuccin-latte', or 'catppuccin-frappe'.
+      vim.cmd.colorscheme 'catppuccin'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
