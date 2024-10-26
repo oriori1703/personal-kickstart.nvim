3d33cf947ec821275de6bf705e9a230fc0786ee1
orip <oriori1703@gmail.com>
2024-10-26 11:51:39 +0300

    Switch from tokyonight to catppuccin

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+local function gh(repo) return 'https://github.com/' .. repo end
+
+-- [[ Colorscheme ]]
+-- You can easily change to a different colorscheme.
+-- Change the name of the colorscheme plugin below, and then
+-- change the command under that to load whatever the name of that colorscheme is.
+--
+-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+vim.pack.add { { src = gh 'catppuccin/nvim', name = 'catppuccin' } }
+
+-- Load the colorscheme here.
+-- Like many other themes, this one has different styles, and you could load
+-- any other, such as 'catppuccin-mocha', 'catppuccin-latte', or 'catppuccin-frappe'.
+vim.cmd.colorscheme 'catppuccin'
+
+-- vim: ts=2 sts=2 sw=2 et
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,7 @@
 require 'kickstart.plugins.guess-indent'
 require 'kickstart.plugins.gitsigns'
 require 'kickstart.plugins.which-key'
-require 'kickstart.plugins.tokyonight'
+-- require 'kickstart.plugins.tokyonight'
 require 'kickstart.plugins.todo-comments'
 require 'kickstart.plugins.mini'
 require 'kickstart.plugins.telescope'
@@ -29,8 +29,8 @@ require 'kickstart.plugins.treesitter'
 
 -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
 --
--- For independent modules, uncomment the convenience loader:
--- require 'custom.plugins'
+-- Load independent modules through the convenience loader:
+require 'custom.plugins'
 --
 -- `custom.plugins` automatically loads files from that directory, but their
 -- order is unspecified. If plugins depend on each other, keep them in the same
