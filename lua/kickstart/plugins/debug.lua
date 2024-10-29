02b1f22f46ed5dac74b7087abbde594af4ea405a
orip <oriori1703@gmail.com>
2024-10-30 00:47:52 +0200

    Add a keybind to create a Logpoint

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -96,6 +96,13 @@ return {
       end,
       desc = 'Debug: Edit Breakpoint',
     },
+    {
+      '<Leader>lp',
+      function()
+        require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+      end,
+      desc = 'Debug: Set Logpoint',
+    },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     { '<F7>', function() require('dapui').toggle() end, desc = 'Debug: See last session result.' },
   },
