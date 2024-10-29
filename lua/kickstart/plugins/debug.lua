103c6e0761e828474802190e8b088bd87b710417
orip <oriori1703@gmail.com>
2024-10-30 00:41:30 +0200

    Add icons for breakpoints

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -141,6 +141,12 @@ dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 dap.listeners.before.event_terminated['dapui_config'] = dapui.close
 dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
+vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '⭕', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointRejected', { text = '🚫', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapLogPoint', { text = '', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '', texthl = '', linehl = '', numhl = '' })
+
 -- Install golang specific config
 -- require('dap-go').setup {
 --   delve = {
