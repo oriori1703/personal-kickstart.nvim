51752d7d13f1a4bb83cd8730600eedebdc2cc9d1
orip <oriori1703@gmail.com>
2024-11-08 17:30:31 +0200

    Turn inlay hints on by default

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -94,6 +94,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- This may be unwanted, since they displace some of your code
     if client and client:supports_method('textDocument/inlayHint', event.buf) then
       map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+
+      vim.lsp.inlay_hint.enable()
     end
   end,
 })
