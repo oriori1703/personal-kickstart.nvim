11d32896ced6e0f7aeb7de3db87f2f85aea5f67b
orip <oriori1703@gmail.com>
2025-03-14 19:01:42 +0200

    Enable linting

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -206,6 +206,7 @@ require('mason-lspconfig').setup {
 local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
   -- You can add other tools here that you want Mason to install
+  'markdownlint',
 })
 
 require('mason-tool-installer').setup { ensure_installed = ensure_installed }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,7 +23,7 @@ require 'kickstart.plugins.treesitter'
 --
 require 'kickstart.plugins.debug'
 require 'kickstart.plugins.indent_line'
--- require 'kickstart.plugins.lint'
+require 'kickstart.plugins.lint'
 require 'kickstart.plugins.autopairs'
 require 'kickstart.plugins.neo-tree'
 
