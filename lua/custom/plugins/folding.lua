02ed015a242d58442039aae196d6a833fb17cac6
orip <oriori1703@gmail.com>
2025-03-14 18:37:39 +0200

    Fix folding for yamlls

--- lua/custom/plugins/folding.lua
+++ lua/custom/plugins/folding.lua
@@ -5,6 +5,17 @@ return {
   init = function()
     vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.opt.foldlevelstart = 99
+
+    vim.lsp.config('*', {
+      capabilities = {
+        textDocument = {
+          foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+          },
+        },
+      },
+    })
   end,
   opts = {},
 }
