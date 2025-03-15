e523bb83afc2f1d4c2f87fe76f49f970c3021e6f
orip <oriori1703@gmail.com>
2025-03-15 23:53:16 +0200

    Add typescript support

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -232,8 +232,9 @@ return {
           --    https://github.com/pmizio/typescript-tools.nvim
           --
           -- But for many setups, the LSP (`ts_ls`) will work just fine
-          -- ts_ls = {},
-          --
+          ts_ls = {},
+          biome = {},
+
           lua_ls = {
             -- cmd = { ... },
             -- filetypes = { ... },
