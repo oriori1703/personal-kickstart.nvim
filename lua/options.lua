ffdb2833e6a0fea2c6c283b55d6e1b9789379a87
orip <oriori1703@gmail.com>
2025-03-27 21:37:32 +0200

    Turn off confirmation for exiting an unsaved file

--- lua/options.lua
+++ lua/options.lua
@@ -67,7 +67,7 @@ vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+vim.o.confirm = false
 
 -- Disable line wrapping
 vim.o.wrap = false
