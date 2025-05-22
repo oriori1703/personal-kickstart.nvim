bbb788346c34e0fc037ba8913c2d5c047b74dadc
orip <oriori1703@gmail.com>
2025-05-22 21:57:39 +0300

    Enable catppuccin integrations

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,17 +1,28 @@
+--- @module 'lazy'
+--- @type LazySpec
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'catppuccin-mocha', 'catppuccin-latte', or 'catppuccin-frappe'.
-      vim.cmd.colorscheme 'catppuccin'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  --- @module 'catppuccin'
+  --- @type CatppuccinOptions
+  opts = {
+    integrations = {
+      blink_cmp = true,
+      snacks = true,
+      which_key = true,
+      mini = true,
+    },
   },
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'catppuccin-mocha', 'catppuccin-latte', or 'catppuccin-frappe'.
+    vim.cmd.colorscheme 'catppuccin'
+
+    -- You can configure highlights by doing something like:
+    -- vim.cmd.hi 'Comment gui=none'
+  end,
 }
+
 -- vim: ts=2 sts=2 sw=2 et
