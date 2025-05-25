e2c94a97011ddc5c20c88dea1d229c05c08dab9d
orip <oriori1703@gmail.com>
2025-05-25 18:07:34 +0300

    Enable rust analyzer

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -225,7 +225,7 @@ return {
           ruff = {
             on_attach = function(client) client.server_capabilities.hoverProvider = false end,
           },
-          -- rust_analyzer = {},
+          rust_analyzer = {},
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
           -- Some languages (like typescript) have entire language plugins that can be useful:
