1ff493366e2de110cf3346a105d1226bdfd9b397
MN-nagy <mahmoudnagimohamed@gmail.com>
2025-07-15 15:42:06 +0300

    Refactor: Use vim.o and adjust scrolling/wrapping options

--- init.lua
+++ init.lua
@@ -88,13 +88,13 @@ P.S. You can delete this when you're done too. It's your config now! :)
 require 'options'
 
 -- Enable undo/redo changes even after closing and reopening a file
-vim.opt.undofile = true
+vim.o.undofile = true
 
--- Enable smooth scrolling
-vim.opt.smoothscroll = true
+-- Disable line wrapping
+vim.o.wrap = false
 
 -- Highlight max chars per line
--- vim.opt.colorcolumn = '100'
+-- vim.o.colorcolumn = '100'
 
 -- [[ Basic Keymaps ]]
 require 'keymaps'
