0e7a16f09a114bf1c8eb4ce178837d2a97f95c8a
orip <oriori1703@gmail.com>
2025-08-20 21:44:55 +0300

    Don't autofold comments

--- lua/custom/plugins/folding.lua
+++ lua/custom/plugins/folding.lua
@@ -2,6 +2,7 @@
 vim.pack.add { 'https://github.com/chrisgrieser/nvim-origami' }
 require('origami').setup {
   foldKeymaps = { setup = false },
+  autoFold = { enabled = true, kinds = { 'imports' } },
 }
 
 -- recommended: disable vim's auto-folding
