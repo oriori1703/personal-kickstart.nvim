3d1d709813c4cd0e03440d018d890c232aa40d00
orip <oriori1703@gmail.com>
2025-09-04 20:12:43 +0300

    Improve the mirroring script

--- mirror.sh
+++ mirror.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+mirror_mason_registry(){
+	target_dir="$mirror_dir/mason-registry"
+	git clone --mirror https://github.com/mason-org/mason-registry.git $target_dir || return
+	cd "$target_dir" || return
+	git config --bool core.bare false
+}
+
+mirror_nvim_plugin(){
+	echo "Processing: $full_name"
+
+	cd "$full_name" || return
+
+	local target_dir
+	target_dir="$mirror_dir/$(basename "$full_name")"
+
+	echo "Cloning mirror to: $target_dir"
+	git clone --mirror "$(git remote get-url origin)" "$target_dir" || return
+
+	cd "$target_dir" || return
+	git config --bool core.bare false
+}
+
 mirror_nvim_plugins() {
 	local plugin_dir="$HOME/.local/share/nvim/lazy"
 	local mirror_dir="/tmp/mirror/plugins"
@@ -7,21 +29,10 @@ mirror_nvim_plugins() {
 	mkdir -p "$mirror_dir"
 
 	find "$plugin_dir" -mindepth 1 -maxdepth 1 | while read -r full_name; do
-		echo "Processing: $full_name"
-
-		cd "$full_name" || continue
-
-		local target_dir
-		target_dir="$mirror_dir/$(basename "$full_name")"
-
-		echo "Cloning mirror to: $target_dir"
-		git clone --mirror "$(git remote get-url origin)" "$target_dir" || continue
-
-		cd "$target_dir" || continue
-		git config --bool core.bare false
-
-		git checkout "$(git rev-parse --abbrev-ref HEAD)"
+		mirror_nvim_plugin $full_name
 	done
+
+	mirror_mason_registry
 }
 
 rm -rf /tmp/mirror.zip || true
