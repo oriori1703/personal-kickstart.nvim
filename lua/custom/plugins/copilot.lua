ef4f35771a33d1621671c8f9071dfa1908ee4f53
orip <oriori1703@gmail.com>
2025-09-23 16:44:35 +0300

    Enable copilot's next edit suggestion

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,11 +2,31 @@
 --- @type LazySpec
 return {
   'zbirenbaum/copilot.lua',
+  dependencies = {
+    {
+      'copilotlsp-nvim/copilot-lsp',
+      --- @module 'copilot-lsp'
+      --- @type copilotlsp.config
+      --- @diagnostic disable-next-line: missing-fields
+      opts = {},
+    },
+  },
   cmd = 'Copilot',
   event = 'InsertEnter',
-  opts = {
-    suggestion = { enable = false },
-    panel = { enabled = false },
-    filetypes = { markdown = true }, -- override the default for markdown
+  --- @module 'copilot'
+  --- @type CopilotConfig
+  opts = { --- @diagnostic disable-line: missing-fields
+    suggestion = { enable = false }, --- @diagnostic disable-line: missing-fields
+    panel = { enabled = false }, --- @diagnostic disable-line: missing-fields
+    filetypes = { markdown = true, help = true }, -- override the default for markdown
+    nes = { --- @diagnostic disable-line: missing-fields
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept_and_goto = '<leader>p',
+        accept = false,
+        dismiss = '<Esc>',
+      },
+    },
   },
 }
