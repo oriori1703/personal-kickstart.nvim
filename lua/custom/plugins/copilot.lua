1a242e2a267f37c19f046b79f99b3499183586a0
orip <oriori1703@gmail.com>
2025-09-23 16:44:35 +0300

    Enable copilot's next edit suggestion

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,18 @@
-vim.pack.add { 'https://github.com/zbirenbaum/copilot.lua' }
+vim.pack.add {
+  'https://github.com/zbirenbaum/copilot.lua',
+  'https://github.com/copilotlsp-nvim/copilot-lsp',
+}
 require('copilot').setup {
   suggestion = { enable = false },
   panel = { enabled = false },
-  filetypes = { markdown = true }, -- override the default for markdown
+  filetypes = { markdown = true, help = true }, -- override the default for markdown
+  nes = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept_and_goto = '<leader>p',
+      accept = false,
+      dismiss = '<Esc>',
+    },
+  },
 }
