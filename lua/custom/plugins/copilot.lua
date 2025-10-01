7816f99c78e3d5aee413ec9244c71993adc048db
Ori Perry <oriori1703@gmail.com>
2025-10-01 20:07:46 +0300

    Switch from copilot-lsp to sidekick.nvim

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,18 +1,27 @@
 vim.pack.add {
+  'https://github.com/folke/sidekick.nvim',
   'https://github.com/zbirenbaum/copilot.lua',
-  'https://github.com/copilotlsp-nvim/copilot-lsp',
 }
+
 require('copilot').setup {
   suggestion = { enable = false },
   panel = { enabled = false },
-  filetypes = { markdown = true, help = true }, -- override the default for markdown
-  nes = {
-    enabled = true,
-    auto_trigger = true,
-    keymap = {
-      accept_and_goto = '<leader>p',
-      accept = false,
-      dismiss = '<Esc>',
-    },
-  },
+  filetypes = { markdown = true, help = true },
 }
+
+require('sidekick').setup {
+  cli = { mux = { backend = 'tmux', enabled = true } },
+}
+
+vim.keymap.set('n', '<tab>', function()
+  if not require('sidekick').nes_jump_or_apply() then return '<Tab>' end
+end, { expr = true, desc = 'Goto/Apply Next Edit Suggestion' })
+vim.keymap.set({ 'n', 'x', 'i', 't' }, '<c-.>', function() require('sidekick.cli').focus() end, { desc = 'Sidekick Switch Focus' })
+vim.keymap.set({ 'n', 'v' }, '<leader>aa', function() require('sidekick.cli').toggle { focus = true } end, { desc = 'Sidekick Toggle CLI' })
+vim.keymap.set(
+  { 'n', 'v' },
+  '<leader>ao',
+  function() require('sidekick.cli').toggle { name = 'opencode', focus = true } end,
+  { desc = 'Sidekick OpenCode Toggle' }
+)
+vim.keymap.set({ 'n', 'v' }, '<leader>ap', function() require('sidekick.cli').select_prompt() end, { desc = 'Sidekick Ask Prompt' })
