cedacb8b6b0a1033b988dccb091963abdc5fe716
Ori Perry <oriori1703@gmail.com>
2025-10-01 20:07:46 +0300

    Switch from copilot-lsp to sidekick.nvim

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,32 +1,59 @@
 --- @module 'lazy'
 --- @type LazySpec
 return {
-  'zbirenbaum/copilot.lua',
-  dependencies = {
+  'folke/sidekick.nvim',
+  ---@module "sidekick"
+  ---@type sidekick.Config
+  opts = {
+    cli = { mux = { backend = 'tmux', enabled = true } },
+  },
+  keys = {
     {
-      'copilotlsp-nvim/copilot-lsp',
-      --- @module 'copilot-lsp'
-      --- @type copilotlsp.config
-      --- @diagnostic disable-next-line: missing-fields
-      opts = {},
+      '<tab>',
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require('sidekick').nes_jump_or_apply() then
+          return '<Tab>' -- fallback to normal tab
+        end
+      end,
+      expr = true,
+      desc = 'Goto/Apply Next Edit Suggestion',
+    },
+    {
+      '<c-.>',
+      function() require('sidekick.cli').focus() end,
+      mode = { 'n', 'x', 'i', 't' },
+      desc = 'Sidekick Switch Focus',
+    },
+    {
+      '<leader>aa',
+      function() require('sidekick.cli').toggle { focus = true } end,
+      desc = 'Sidekick Toggle CLI',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>ao',
+      function() require('sidekick.cli').toggle { name = 'opencode', focus = true } end,
+      desc = 'Sidekick OpenCode Toggle',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>ap',
+      function() require('sidekick.cli').select_prompt() end,
+      desc = 'Sidekick Ask Prompt',
+      mode = { 'n', 'v' },
     },
   },
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  --- @module 'copilot'
-  --- @type CopilotConfig
-  opts = { --- @diagnostic disable-line: missing-fields
-    suggestion = { enable = false }, --- @diagnostic disable-line: missing-fields
-    panel = { enabled = false }, --- @diagnostic disable-line: missing-fields
-    filetypes = { markdown = true, help = true }, -- override the default for markdown
-    nes = { --- @diagnostic disable-line: missing-fields
-      enabled = true,
-      auto_trigger = true,
-      keymap = {
-        accept_and_goto = '<leader>p',
-        accept = false,
-        dismiss = '<Esc>',
-      },
+  dependencies = {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    --- @module 'copilot'
+    --- @type CopilotConfig
+    opts = { --- @diagnostic disable-line: missing-fields
+      suggestion = { enable = false }, --- @diagnostic disable-line: missing-fields
+      panel = { enabled = false }, --- @diagnostic disable-line: missing-fields
+      filetypes = { markdown = true, help = true }, -- override the default for markdown
     },
   },
 }
