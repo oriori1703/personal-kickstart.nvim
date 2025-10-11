5927532918de1db78f470bb6203bea93f8de7b4e
Ori Perry <oriori1703@gmail.com>
2025-10-11 15:00:50 +0300

    Update the keymaps of sidekick.nvim to match their new defaults

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -16,12 +16,12 @@ require('sidekick').setup {
 vim.keymap.set('n', '<tab>', function()
   if not require('sidekick').nes_jump_or_apply() then return '<Tab>' end
 end, { expr = true, desc = 'Goto/Apply Next Edit Suggestion' })
-vim.keymap.set({ 'n', 'x', 'i', 't' }, '<c-.>', function() require('sidekick.cli').focus() end, { desc = 'Sidekick Switch Focus' })
-vim.keymap.set({ 'n', 'v' }, '<leader>aa', function() require('sidekick.cli').toggle { focus = true } end, { desc = 'Sidekick Toggle CLI' })
-vim.keymap.set(
-  { 'n', 'v' },
-  '<leader>ao',
-  function() require('sidekick.cli').toggle { name = 'opencode', focus = true } end,
-  { desc = 'Sidekick OpenCode Toggle' }
-)
-vim.keymap.set({ 'n', 'v' }, '<leader>ap', function() require('sidekick.cli').select_prompt() end, { desc = 'Sidekick Ask Prompt' })
+vim.keymap.set({ 'n', 't', 'i', 'x' }, '<c-.>', function() require('sidekick.cli').toggle() end, { desc = 'Sidekick Toggle' })
+vim.keymap.set('n', '<leader>aa', function() require('sidekick.cli').toggle() end, { desc = 'Sidekick Toggle CLI' })
+vim.keymap.set('n', '<leader>as', function() require('sidekick.cli').select() end, { desc = 'Select CLI' })
+vim.keymap.set('n', '<leader>ad', function() require('sidekick.cli').close() end, { desc = 'Detach a CLI Session' })
+vim.keymap.set({ 'x', 'n' }, '<leader>at', function() require('sidekick.cli').send { msg = '{this}' } end, { desc = 'Send This' })
+vim.keymap.set('n', '<leader>af', function() require('sidekick.cli').send { msg = '{file}' } end, { desc = 'Send File' })
+vim.keymap.set('x', '<leader>av', function() require('sidekick.cli').send { msg = '{selection}' } end, { desc = 'Send Visual Selection' })
+vim.keymap.set({ 'n', 'x' }, '<leader>ap', function() require('sidekick.cli').prompt() end, { desc = 'Sidekick Select Prompt' })
+vim.keymap.set('n', '<leader>ao', function() require('sidekick.cli').toggle { name = 'opencode', focus = true } end, { desc = 'Sidekick Toggle Claude' })
