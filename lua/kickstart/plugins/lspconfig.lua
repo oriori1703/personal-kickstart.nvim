8cc179ba3ca2d10776850bb2ad5be64dcb434470
orip <oriori1703@gmail.com>
2025-10-28 22:11:33 +0200

    Add clippy to rust-analyzer

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -225,7 +225,15 @@ return {
           ruff = {
             on_attach = function(client) client.server_capabilities.hoverProvider = false end,
           },
-          rust_analyzer = {},
+          rust_analyzer = {
+            settings = {
+              ['rust-analyzer'] = {
+                check = {
+                  command = 'clippy',
+                },
+              },
+            },
+          },
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
           -- Some languages (like typescript) have entire language plugins that can be useful:
