fc5d362138387bb6af5d0a4f67e515b16a05a82d
orip <oriori1703@gmail.com>
2025-11-15 22:46:37 +0200

    Add rename hook for fyler.nvim

--- lua/custom/plugins/fyler.lua
+++ lua/custom/plugins/fyler.lua
@@ -8,9 +8,12 @@ return {
       enabled = vim.g.have_nerd_font,
       config = function() require('mini.icons').setup {} end,
     },
+    'folke/snacks.nvim',
   },
   --- @type FylerSetup
-  opts = {},
+  opts = {
+    hooks = { on_rename = function(src_path, dest_path) Snacks.rename.on_rename_file(src_path, dest_path) end },
+  },
   keys = {
     { '\\', function() require('fyler').toggle { kind = 'split_left_most' } end, desc = 'File Explorer' },
   },
