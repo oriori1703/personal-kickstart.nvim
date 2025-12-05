69b0619d1327d31b67fd982a1500e0fd119a804b
orip <oriori1703@gmail.com>
2025-12-05 21:53:06 +0200

    Add AnsiColorize user command
    The command is used to render the current buffer inside a terminal with ANSI colors

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -93,4 +93,29 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
+local function ansi_colorize()
+  -- https://vi.stackexchange.com/a/45683
+  vim.opt.laststatus = 0
+
+  vim.wo.number = false
+  vim.wo.relativenumber = false
+  vim.wo.statuscolumn = ''
+  vim.wo.signcolumn = 'no'
+  vim.opt.listchars = { space = ' ' }
+
+  local buf = vim.api.nvim_get_current_buf()
+
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  while #lines > 0 and vim.trim(lines[#lines]) == '' do
+    lines[#lines] = nil
+  end
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+
+  vim.api.nvim_chan_send(vim.api.nvim_open_term(buf, {}), table.concat(lines, '\r\n'))
+  vim.keymap.set('n', 'q', '<cmd>qa!<cr>', { silent = true, buffer = buf })
+  vim.api.nvim_create_autocmd('TextChanged', { buffer = buf, command = 'normal! G$' })
+  vim.api.nvim_create_autocmd('TermEnter', { buffer = buf, command = 'stopinsert' })
+end
+vim.api.nvim_create_user_command('AnsiColorize', ansi_colorize, { desc = 'Render the current buffer inside a terminal with ANSI colors' })
+
 -- vim: ts=2 sts=2 sw=2 et
