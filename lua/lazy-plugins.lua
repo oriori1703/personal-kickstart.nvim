0659764e35c4ca0957712837e99044077d6273c7
Ori Perry <oriori1703@gmail.com>
2026-03-22 00:32:35 +0200

    Create a mirroring script

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -58,9 +58,9 @@ require('lazy').setup({
   { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
+  -- Or use snacks-picker!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+  -- you can continue same window with `<space>sr` which resumes last snacks-picker search
 }, { ---@diagnostic disable-line: missing-fields
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -82,7 +82,7 @@ require('lazy').setup({
     },
   },
   git = {
-    filter = false, -- Disable --filter for git clone to make it easier to copy the plugins for offline use
+    filter = true, -- Disable --filter for git clone to make it easier to copy the plugins for offline use
     url_format = 'https://github.com/%s.git', -- Explicitly use the default URL format to make it easier to change on corporate networks
   },
 })
