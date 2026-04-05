dd1d93fb0352397195ebbabf27aeb8cc3ab399c7
orip <oriori1703@gmail.com>
2026-04-05 15:03:02 +0300

    Use the new api to open an existing buffer in the terminal for AnsiColorize

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -95,8 +95,13 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 
 local function ansi_colorize()
   -- https://vi.stackexchange.com/a/45683
-  vim.opt.laststatus = 0
+  vim.o.fixendofline = false
+  vim.o.endofline = false
 
+  vim.o.laststatus = 0
+  vim.o.cmdheight = 0
+
+  vim.wo.scrolloff = 0
   vim.wo.number = false
   vim.wo.relativenumber = false
   vim.wo.statuscolumn = ''
@@ -104,15 +109,9 @@ local function ansi_colorize()
   vim.opt.listchars = { space = ' ' }
 
   local buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_open_term(buf, {})
 
-  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-  while #lines > 0 and vim.trim(lines[#lines]) == '' do
-    lines[#lines] = nil
-  end
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
-
-  vim.api.nvim_chan_send(vim.api.nvim_open_term(buf, {}), table.concat(lines, '\r\n'))
-  vim.keymap.set('n', 'q', '<cmd>qa!<cr>', { silent = true, buffer = buf })
+  vim.keymap.set('n', 'q', '<cmd>q<cr>', { silent = true, buffer = buf })
   vim.api.nvim_create_autocmd('TextChanged', { buffer = buf, command = 'normal! G$' })
   vim.api.nvim_create_autocmd('TermEnter', { buffer = buf, command = 'stopinsert' })
 end
