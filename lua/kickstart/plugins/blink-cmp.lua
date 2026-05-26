oriori1703/kickstart-modular.nvim

Switch from copilot.vim to copilot.lua and blink-cmp-copilot

lua/kickstart/plugins/blink-cmp.lua
@@ -31,6 +31,7 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
+      'giuxtaposition/blink-cmp-copilot',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -76,9 +77,15 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'copilot' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            score_offset = 100,
+            async = true,
+          },
         },
       },
 

oriori1703/personal-kickstart.nvim

lua/kickstart/plugins/blink-cmp.lua
@@ -28,6 +28,7 @@ return {
         },
         opts = {},
       },
+      'giuxtaposition/blink-cmp-copilot',
     },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
@@ -73,7 +74,7 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
         providers = {
           buffer = {
             -- Make buffer compeletions appear at the end.
@@ -87,6 +88,12 @@ return {
               local filetype = vim.bo.filetype
               return vim.tbl_contains(enabled_filetypes, filetype)
             end,
+          },
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            score_offset = 100,
+            async = true,
           },
         },
       },