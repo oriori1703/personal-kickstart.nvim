5b018992cbdc386a9de3a1122f592bd0f2399fbb
orip <oriori1703@gmail.com>
2026-06-19 13:44:35 +0300

    Fix fyler config for the latest version

--- lua/custom/plugins/fyler.lua
+++ lua/custom/plugins/fyler.lua
@@ -3,5 +3,9 @@ vim.pack.add { 'https://github.com/A7Lavinraj/fyler.nvim' }
 local fyler = require 'fyler'
 fyler.setup {
   hooks = { on_rename = function(src_path, dest_path) Snacks.rename.on_rename_file(src_path, dest_path) end },
+  integrations = { icon = 'mini_icons' },
+  extensions = {
+    git = { enabled = true },
+  },
 }
 vim.keymap.set('n', '\\', function() fyler.toggle { kind = 'split_left_most' } end, { desc = 'File Explorer' })
